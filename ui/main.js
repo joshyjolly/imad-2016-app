4461e0d383a1ce7ee46dd44cb71141dae578a715
joshyjolly <joshykjolly@gmail.com>
2016-09-28 13:26:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,16 @@ var button=document.getElementById('bt');
 var bt=0;
 button.onclick = function ()
 {
-    bt=bt+1;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function(){
+    if(request.readystate===XMLHttpRequest.DONE){
+        if (request.status===200){
+            var counter=request.responseText;
     var span=document.getElementById('st');
-   span.innerHTML=bt.toString();
+   span.innerHTML= bt.toString();
+        }
+    }
+    };
+    request.open('GET','http://joshyjolly.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
