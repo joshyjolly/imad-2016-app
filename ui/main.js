a47d1ac1dd36c446d25aa7acfc83e34861f81d0e
joshyjolly <joshykjolly@gmail.com>
2016-09-28 11:11:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 
-var button=document.getElementById('b');
-var b=0;
+var button=document.getElementById('bt');
+var bt=0;
 button.onclick = function ()
 {
-    b=b+1;
-    var span=document.getElementById('s');
-   span.innerHTML=b.toString();
+    bt=bt+1;
+    var span=document.getElementById('st');
+   span.innerHTML=bt.toString();
 };
