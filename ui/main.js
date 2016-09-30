b1a5a504264986a89042b962e576dfb4c92255fd
joshyjolly <joshykjolly@gmail.com>
2016-09-30 20:29:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick = function ()
     if(request.readystate===XMLHttpRequest.DONE){
         if (request.status===200){
             var counter=request.responseText;
-    var span=document.getElementById('st');
+    var span=document.getElementById('count');
    span.innerHTML= counter.toString();
         }
     }
