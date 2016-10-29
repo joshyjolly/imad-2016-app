4ca6cbc8c46d6a3cd3d18834d6151d308c1d89eb
joshyjolly <joshykjolly@gmail.com>
2016-10-29 09:07:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
-
- function f()
-{
-    var request = new XMLHttpRequest();
-    request.onreadystatechange=function(){
-    if(request.readystate===XMLHttpRequest.DONE){
-        if (request.status===200){
-            var counter=request.responseText;
-    return(counter.toString());
-        }
-    }
-    };
-    request.open('GET','http://joshyjolly.imad.hasura-app.io/conter',true);
-    request.send(null);
-}
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet">
+    </head>
+    <body>
+         <div class ="container">
+             <marquee dir="right"><img src="/ui/madi.png" class="img-medium"/></marquee>
+            </div>
+        <br>
+              
+            <div class="center text-big bold">
+             Hi! This is my first webapp!!!!!!!!!!!!!!!
+            </div>
+        <div class="content">
+       <a href="/Myself.html">Myself</a><br>
+       <a href="google.co.in">google</a>
+            <script type="text/javascript" src="/ui/main.js">
+            </script>
+           <br><br><br> <button id="counter" onclick="f()"> click </button> clicked <span id="count"> 0 </span> times
+            </div>
+        </body>
+</html>
