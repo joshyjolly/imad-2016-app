dc32782f9b539b04efb36a490b8756ae5a47dea4
joshyjolly <joshykjolly@gmail.com>
2016-10-29 09:34:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/Myself.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'Myself.html'));
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui','style.cs'));
+  res.sendFile(path.join(__dirname, 'ui','style.css'));
 });
 
 app.get('/login.html', function (req, res) {
